fc3adcf7eebbe660811d10bcb6d2a3bc050619d4
Mihai Isarescu <mihai.isarescu@protonmail.com>
2023-12-10 21:50:50 +0200

    Update client to join rooms via cli flags

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "chat-app/internal/client"
+import (
+	"chat-app/internal/client"
+	"flag"
+)
 
 func main() {
-	client.RunClient()
+	roomId := flag.Int("room", 0, "room id to be joined")
+	userId := flag.Int("userid", 0, "user id to join")
+
+	flag.Parse()
+
+	c := client.NewClient("ws://localhost:8080/chatroom",*roomId,*userId,)
+	
+	c.RunClient()
 }
